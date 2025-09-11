83562f4f64ed656d10fcca3dd8e4f26c0251695b
RaviGupta965 <clicktoravi1234@gmail.com>
2025-09-11 22:58:44 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { supabaseClient } from './lib/supabase_client'
+
+export default function page() {
+  const [session, setSession] = useState<any>(null)
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data: { session } } = await supabaseClient.auth.getSession()
+      setSession(session)
+    }
+    getSession();
+  }, [])
+
+  if (!session) {
+    return <p>You must be logged in to view this page.</p>
+  }
+
+  return <h1>Welcome {JSON.stringify(session)}</h1>
+}
---
+++ src/app/utils/Auth.ts
@@ -0,0 +1,15 @@
+import { supabaseClient } from '@/app/lib/supabase_client'
+
+export const signUp = async (email :string,password:string) => {
+  const { data, error } = await supabaseClient.auth.signUp({ email, password })
+  return { data, error }
+}
+
+export const signIn = async (email:string, password:string) => {
+  const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password })
+  return { data, error }
+}
+
+export const signOut = async () => {
+  await supabaseClient.auth.signOut()
+}
